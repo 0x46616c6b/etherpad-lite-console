ee0335eeaf816095c957126948cba52d4783e0c9
louis <louis@systemli.org>
2014-01-02 16:08:41 +0100

    refactored name

--- src/EtherpadLite/Console/Command/PurgePadCommand.php
+++ src/EtherpadLite/Console/Command/PurgePadCommand.php
@@ -10,7 +10,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class DeleteOldPadsCommand extends Command
+class PurgePadCommand extends Command
 {
     protected function configure()
     {
